04f6b19288d26a521beca4f018cf4f0fac6df171
R AMRUTHA <ammuamrutha26661@gmail.com>
2026-04-09 18:30:00 +0530

    Time: 2 ms (75.75%) | Memory: 43.9 MB (69.33%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String clearDigits(String s) {
+        int charIndex=0;
+        StringBuilder sb=new StringBuilder(s);
+        while(charIndex<sb.length()){
+            if(Character.isDigit(sb.charAt(charIndex))){
+                sb.deleteCharAt(charIndex);
+                sb.deleteCharAt(charIndex-1);
+                charIndex--;
+            }
+            else{
+                charIndex++;
+            }
+        }
+        return sb.toString();
+        }
+    }
